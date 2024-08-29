a71058397929dc1c843df48d9fcc374c45520b83
Win11 Pro 23H2 <Win11 Pro 23H2@192.168.1.54>
2024-08-29 15:30:28 +0530

    all array programs

---
+++ javaproject1/src/javaproject1/Naresh.java
@@ -0,0 +1,14 @@
+package javaproject1;
+
+import java.util.Scanner;
+
+public class Naresh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc= new Scanner(System.in);
+    String s= sc.next();
+    System.out.println(s);
+	}
+
+}
